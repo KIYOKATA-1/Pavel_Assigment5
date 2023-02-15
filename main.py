8da615245f9f31fc09eba7c102607402b12ef9ad
Me <park.pavel04@gmail.com>
2023-02-16 00:01:18 +0600

    Create Menu

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import os
 #Create File:
 
 file = "file.txt"
+
+#Create Menu with Options:
+
+menu = {
+  "1": "Create File",
+  "2": "Add Data",
+  "3": "Read",
+  "4": "Rename File",
+  "5": "Delete",
+  "6": "Exit"
+}
